94ecb89aabb9920107f5634928933b65550ef7fd
YuraSadilenko <yura.sadilenko.it@gmail.com>
2019-01-17 15:02:00 +0200

    fixes

--- script/numbers.js
+++ script/numbers.js
@@ -1,7 +1,7 @@
 getNumber();
 
 function getNumber() {
-  var numbers = '1 + 2';
+  var numbers = '20 / 55';
   var regexp = /(-?\d+(?:\.\d+)?)\s*([-+*\/])\s*(-?\d+(?:\.\d+)?)/;
   console.log(numbers.match(regexp));
 }
--- script/time.js
+++ script/time.js
@@ -1,6 +1,6 @@
 function getTime() {
-  var dinner = 'dinner at 9:00';
-  var regexp = /2[0-3]|[0-1]*\d:[0-5]\d/;
+  var regexp = /2[0-3]:[0-5]\d|[0-1]*\d:[0-5]\d/;
+  var dinner = 'dinner at 20:55';
   console.log(dinner.match(regexp));
 }
 
